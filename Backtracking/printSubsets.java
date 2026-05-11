0db05996cc3a0919a26ba19a3ad212cca8c45bd0
patel priyanshu <patelpriyanshu1209@gmail.com>
2026-05-11 19:16:47 +0530

    subset problem

---
+++ Backtracking/printSubsets.java
@@ -0,0 +1,16 @@
+package Backtracking;
+
+public class printSubsets {
+    public static void Subset(String s,String ans,int i){
+        if(i==s.length()){
+            System.out.println(ans);
+            return;
+        }
+        Subset(s,ans+s.charAt(i),i+1);
+        Subset(s,ans,i+1);
+    }
+    public static void main(String[] args) {
+        String ans="";
+        Subset("abc", ans, 0);
+    }
+}
