b13eb508909bde187bc662c8baf9b554253d76f5
patel priyanshu <patelpriyanshu1209@gmail.com>
2026-05-31 11:58:56 +0530

    queue

---
+++ Queue/circularQueueArray.java
@@ -0,0 +1,73 @@
+package Queue;
+public class circularQueueArray{
+    static class Queue{
+       static int arr[];
+        static int size;
+        static int rear;
+        static int front;
+        public Queue(int n){
+            arr=new int[n];
+            size=n;
+            rear=-1;
+            front=-1;
+        }
+
+        public static boolean isEmpty(){
+            return rear==-1&&front==-1;
+        }
+
+        public static boolean isFull(){
+            return (rear+1)%size==front;
+        }
+
+        //add
+        public static void add(int data){
+            if(isFull()){
+                System.out.println("queue is full");
+                return;
+            }
+            if(front==-1){
+                front=0;
+            }
+            rear=(rear+1)%size;
+            arr[rear]=data;
+        }
+
+        //remove
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("empty queue");
+                return -1;
+            }
+
+
+            int val=arr[front];
+           
+            if(rear==front){
+                rear=-1;
+                front=-1;
+            }else{
+                 front=(front+1)%size;
+            }
+            return val;
+        }
+
+        //peek
+        public static int peek(){
+            return arr[front];
+        }
+    }
+    public static void main(String[] args) {
+        Queue q=new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.remove();
+        q.add(5);
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
