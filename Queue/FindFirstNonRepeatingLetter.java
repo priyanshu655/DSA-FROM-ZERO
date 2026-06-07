340e8436447acfb8a8fbc43c3b1daf64be4f7665
patel priyanshu <patelpriyanshu1209@gmail.com>
2026-06-07 17:45:54 +0530

    queue

---
+++ Queue/FindFirstNonRepeatingLetter.java
@@ -0,0 +1,28 @@
+package Queue;
+import java.util.*;
+
+public class FindFirstNonRepeatingLetter{
+    public static void FirstNonRepLetter(String s){
+        Queue<Character>q=new LinkedList<>();
+        int freq[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+            while(!q.isEmpty()&&freq[q.peek()-'a']>1){
+                q.remove();
+            }
+
+            if(q.isEmpty()){
+                System.out.print(-1+" ");
+            }else{
+                System.out.print(q.peek()+" ");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        String s="aabccxb";
+        FirstNonRepLetter(s);
+    }
+}
