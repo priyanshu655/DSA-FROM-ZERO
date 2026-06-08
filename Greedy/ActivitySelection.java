64cc410c1d9da602b85ba083fa3eebcf2d8c1fad
patel priyanshu <patelpriyanshu1209@gmail.com>
2026-06-08 18:04:25 +0530

    greedy

--- Greedy/ActivitySelection.java
+++ Greedy/ActivitySelection.java
@@ -1,22 +1,28 @@
 package Greedy;
 
+import java.util.ArrayList;
+
 public class ActivitySelection{
-    public static int activiy_selection(int a1[],int a2[]){
+    public static void activiy_selection(int a1[],int a2[]){
         int count=1;
-        int st=a1[0];
+        ArrayList<Integer> ans=new ArrayList<>();
         int et=a2[0];
+        ans.add(0);
         for(int i=1;i<a1.length;i++){  
            if(a1[i]>=et){
+            ans.add(i);
             count++;
-            st=a1[i];
             et=a2[i];
            }
         }
-        return count;
+        System.out.println("Max number of actitivty that can be performed is : "+count);
+        for(int i=0;i<ans.size();i++){
+            System.out.print("A"+ans.get(i)+" ");
+        }
     }
     public static void main(String[] args) {
-        int a1[]={1,3,0,5,8,5};
+        int a1[]={1,3,0,2,8,1};
         int a2[]={2,4,6,7,9,9};
-        System.out.println(activiy_selection(a1, a2));
+        activiy_selection(a1, a2);
     }
 }
