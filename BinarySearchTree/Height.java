0b081b16e92ae6d511896440249a95219b4e1807
patel priyanshu <patelpriyanshu1209@gmail.com>
2026-07-08 12:30:39 +0530

    new cocnept

--- BinarySearchTree/Height.java
+++ BinarySearchTree/Height.java
@@ -43,6 +43,41 @@ public class Height {
         return ls+rs+root.data;
 
     }
+
+    public static int diameter(Node root){
+        if(root==null){
+            return 0;
+        }
+
+        int leftDiam=diameter(root.left);
+        int rightDiam=diameter(root.right);
+        int lh=heightOfTree(root.left);
+        int rh=heightOfTree(root.right);
+        int selfDiam=lh+rh+1;
+        return Math.max(Math.max(leftDiam,rightDiam),selfDiam);
+    }
+
+    static class Info{
+        int diam;
+        int ht;
+        public Info(int diam,int ht){
+            this.diam=diam;
+            this.ht=ht;
+        }
+    }
+
+   public  static  Info diameter2(Node root){
+    if(root==null){
+        return new Info(0,0);
+    }
+    Info leftInfo=diameter2(root.left);
+    Info rightInfo=diameter2(root.right);
+
+    int diam=Math.max(Math.max(leftInfo.diam,rightInfo.diam),leftInfo.ht+rightInfo.ht+1);
+    int ht=Math.max(leftInfo.ht,rightInfo.ht)+1;
+
+    return new Info(diam,ht);
+    }
     public static void main(String[] args) {
         Node root=new Node(1);
         root.left=new Node(2);
@@ -62,5 +97,8 @@ public class Height {
 
        int sumofnodes=sum_of_nodes(root);
        System.out.println("sum of all nodes are : "+sumofnodes);
+
+       int max_diam=diameter(root);
+       System.out.println("diameter od tree is : "+max_diam);
     }
 }
---
+++ BinarySearchTree/subTree.java
@@ -0,0 +1,62 @@
+package BinarySearchTree;
+
+public class subTree {
+    public static class Node{
+        int data;
+        Node right;
+        Node left;
+        public Node(int data){
+            this.data=data;
+            this.right=null;
+            this.left=null;
+        }
+    }
+
+    public static boolean isIdentical(Node root,Node subroot){
+        if(root==null&&subroot==null){
+            return true;
+        }else if(root==null||subroot==null||subroot.data!=root.data){
+            return false;
+        }
+
+      if(!isIdentical(root.left, subroot.left)){
+        return false;
+      }
+
+      if(!isIdentical(root.right, subroot.right)){
+        return false;
+      }
+
+      return true;
+    }
+
+    public static boolean isSubTree(Node root,Node subroot){
+        if(root==null){
+            return false;
+        }
+        if(root.data==subroot.data){
+            if(isIdentical(root,subroot)){
+                return true;
+            }
+        }
+
+        return isSubTree(root.left, subroot) || isSubTree(root.right, subroot);
+    }
+
+    public static void main(String[] args) {
+        Node root=new Node(1);
+        root.left=new Node(2);
+        root.right=new Node(3);
+        root.left.left=new Node(4);
+        root.left.right=new Node(5);
+        root.right.left=new Node(6);
+        root.right.right=new Node(7);
+
+        Node subRoot=new Node(2);
+        subRoot.left=new Node(4);
+        subRoot.right=new Node(5);
+
+       boolean issubTree= isSubTree(root, subRoot);
+       System.out.println(issubTree);
+    }
+}
